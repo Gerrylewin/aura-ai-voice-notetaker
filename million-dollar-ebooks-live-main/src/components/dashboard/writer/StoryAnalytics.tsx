dfd68d2c135942ab6580f036ac4ea0d7f2989617
Isaac Lewin <128657368+Gerrylewin@users.noreply.github.com>
2025-09-01 18:24:23 -0400

    Delete million-dollar-ebooks-live-main directory

--- million-dollar-ebooks-live-main/src/components/dashboard/writer/StoryAnalytics.tsx
+++
@@ -1,7 +0,0 @@
-
-import React from 'react';
-import { StoryAnalyticsContainer } from './analytics/StoryAnalyticsContainer';
-
-export function StoryAnalytics() {
-  return <StoryAnalyticsContainer />;
-}
--- million-dollar-ebooks-live-main/src/components/settings/GoalsSettings.tsx
+++
@@ -1,7 +0,0 @@
-
-import React from 'react';
-import { GoalsForm } from './goals/GoalsForm';
-
-export function GoalsSettings() {
-  return <GoalsForm />;
-}
